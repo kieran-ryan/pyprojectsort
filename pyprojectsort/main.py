5d34fd7ee8a65408a2f0077a099e5444d8876774
Kieran Ryan <kierankilkenny@gmail.com>
2023-06-23 22:27:53 +0100

    Sort project toml section names

---
+++ pyprojectsort/main.py
@@ -0,0 +1,30 @@
+"""pyproject-sort implementation."""
+from __future__ import annotations
+
+import pathlib
+
+import tomli as tomllib
+import tomli_w
+
+with pathlib.Path("pyproject.toml").open("rb") as f:
+    pyproject_toml = tomllib.load(f)
+toml_config = pyproject_toml.get("project")
+project_metadata = {
+    k.replace("--", "").replace("-", "_"): v for k, v in toml_config.items()
+}
+
+
+def reformat_pyproject(pyproject: dict) -> dict:
+    """Reformat pyproject toml file."""
+    sections_sorted = sorted(pyproject)
+    return {i: pyproject[i] for i in sections_sorted}
+
+
+sections_sorted = sorted(pyproject_toml)
+data_dict = {i: pyproject_toml[i] for i in sections_sorted}
+
+with pathlib.Path("pyproject.toml").open("wb") as f:
+    tomli_w.dump(data_dict, f)
+
+if __name__ == "__main__":
+    reformat_pyproject(pyproject_toml)
---
+++ tests/unit/test_main.py
@@ -0,0 +1,22 @@
+"""pyproject-sort unit tests."""
+
+from __future__ import annotations
+
+from pyprojectsort.main import reformat_pyproject
+
+
+def test_reformat_pyproject():
+    """Test pyproject toml is reformatted."""
+    pyproject = {
+        "project": {"name": "pyprojectsort"},
+        "build-system": {"name": "flit"},
+        "tool.black": {"line_length": 88},
+    }
+
+    # TODO: Amend test to validate order
+    sorted_pyproject = {
+        "build-system": {"name": "flit"},
+        "project": {"name": "pyprojectsort"},
+        "tool.black": {"line_length": 88},
+    }
+    assert reformat_pyproject(pyproject) == sorted_pyproject
