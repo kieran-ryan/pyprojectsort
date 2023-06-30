a1c758121162077d76633c9d7d77685612073790
Kieran Ryan <kierankilkenny@gmail.com>
2023-07-01 00:15:09 +0100

    feat: specify pyproject path from cli (#11)

--- pyprojectsort/main.py
+++ pyprojectsort/main.py
@@ -1,17 +1,48 @@
 """pyprojectsort implementation."""
 from __future__ import annotations
 
+import argparse
 import pathlib
+import sys
+from typing import Any
 
 import tomli as tomllib
 import tomli_w
 
-with pathlib.Path("pyproject.toml").open("rb") as f:
-    pyproject_toml = tomllib.load(f)
-toml_config = pyproject_toml.get("project")
-project_metadata = {
-    k.replace("--", "").replace("-", "_"): v for k, v in toml_config.items()
-}
+from pyprojectsort import __version__
+
+DEFAULT_CONFIG = "pyproject.toml"
+
+
+def _read_cli(args: list):
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        prog="pyprojectsort",
+        description="Formatter for pyproject.toml files",
+    )
+    parser.add_argument("file", nargs="?", default=DEFAULT_CONFIG)
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=__version__,
+        help="show package version and exit",
+    )
+    return parser.parse_args(args)
+
+
+def _read_config_file(config: pathlib.Path):
+    """Check configuration file exists."""
+    if not config.is_file():
+        print(f"No pyproject.toml detected at path: '{config}'")
+        sys.exit(1)
+    return config
+
+
+def _parse_pyproject_toml(file: pathlib.Path) -> dict[str, Any]:
+    """Parse pyproject.toml file."""
+    with file.open("rb") as f:
+        pyproject_toml = tomllib.load(f)
+    return {k.replace("--", "").replace("-", "_"): v for k, v in pyproject_toml.items()}
 
 
 def reformat_pyproject(pyproject: dict) -> dict:
@@ -26,8 +57,16 @@ def reformat_pyproject(pyproject: dict) -> dict:
     return pyproject
 
 
+def _save_pyproject(file, pyproject) -> None:
+    """Write changes to pyproject.toml."""
+    with file.open("wb") as f:
+        tomli_w.dump(pyproject, f)
+
+
 def main():
     """Run application."""
-    data_dict = reformat_pyproject(pyproject_toml)
-    with pathlib.Path("pyproject.toml").open("wb") as f:
-        tomli_w.dump(data_dict, f)
+    args = _read_cli(sys.argv[1:])
+    pyproject_file = _read_config_file(pathlib.Path(args.file))
+    pyproject_toml = _parse_pyproject_toml(pyproject_file)
+    reformatted_pyproject = reformat_pyproject(pyproject_toml)
+    _save_pyproject(pyproject_file, reformatted_pyproject)
